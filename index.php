7f0fcb48d402f0fc5dc6560649bb2165d67bd6ae
BuildTools <kabirdesarkar@gmail.com>
2018-06-27 10:01:13 +0530

    Restaged environment for ajax requests

--- index.php
+++ index.php
@@ -1,32 +1,4 @@
 <?php
-// if type = register
-if($_SERVER["REQUEST_METHOD"] == "POST") {
-  $prompt = "";
-  $usr = $_POST["usr"]; // receive variable 'usr'
-  $display = $_POST["display"]; // receive variable 'display'
-  $pwd  = $_POST["pwd"];//  receive variable 'pwd'
-  if (strlen($usr) < 6 || strlen($display) < 6 || strlen($pwd) < 6) { // if Character expectation less then:
-    $prompt = "Username, display and password must be at least 6 characters";
-  }
-  else { // otherwise:
-    $conn = new mysqli("localhost","root","root","project"); // use OOP style to establish connection;
-    $query = "SELECT * FROM users WHERE usr='$usr'";
-    $res = $conn->query($query); // Query the $query variable
-    $check = $res->num_rows; // Calculate number of received rows as $check
-    if ($check == 1) {//if user exists
-      $prompt = "User with username ".$usr." already exists. If you want to login, <a href='/login.php'>Go here</a>";
-    }
-    if ($check == 0) {//if not
-      $query = "INSERT INTO users (usr,display,pwd) VALUES ('$usr','$display','$pwd')"; // insert user query
-      $res = $conn->query($query); // execut $query and store in $res just in case
-      $_SESSION["usr"] = $usr;
-      $_SESSION["display"] = $display;
-      $_SESSION["pwd"] = $pwd;
-      header("location:Session/session.php");
-    }
-  }
-}
-
 
  ?>
 
@@ -48,13 +20,13 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
   </head>
   <body>
     <center>
-      <form action="index.php" method="POST">
+      <form action="process.php" method="POST">
         <center>
           Register now!<br><br>
           <input type="text" name="usr" placeholder="Username*" required><br>
           <input type="text" name="display" placeholder="Dispay name*" required><br>
           <input type="password" name="pwd" placeholder="Password*" required><br>
-          <div class="p"><?php echo $prompt ?></div>
+          <div class="p"></div>
            <br><br>
           <input type="submit" name="type" value="register"><br><br>
           Already have an account?<br>
--- process.php
+++ process.php
@@ -11,7 +11,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") { // if method is post
     $display = $_POST["display"]; // receive variable 'display'
     $pwd  = $_POST["pwd"];//  receive variable 'pwd'
     if (strlen($usr) < 6 || strlen($display) < 6 || strlen($pwd) < 6) { // if Character expectation less then:
-      $prompt = "Username, display and password must be at least 6 characters";
+      echo "Username, display and password must be at least 6 characters";
     }
     else { // otherwise:
       $conn = new mysqli("localhost","root","root","project"); // use OOP style to establish connection;
@@ -19,7 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") { // if method is post
       $res = $conn->query($query); // Query the $query variable
       $check = $res->num_rows; // Calculate number of received rows as $check
       if ($check == 1) {//if user exists
-        $prompt = "User with username ".$usr." already exists. If you want to login, <a href='/login.php'>Go here</a>";
+        echo "User with username ".$usr." already exists. If you want to login, <a href='/login.php'>Go here</a>";
       }
       if ($check == 0) {//if not
         $query = "INSERT INTO users (usr,display,pwd) VALUES ('$usr','$display','$pwd')"; // insert user query
@@ -27,7 +27,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") { // if method is post
         $_SESSION["usr"] = $usr;
         $_SESSION["display"] = $display;
         $_SESSION["pwd"] = $pwd;
-        $prompt = "Successfully registered user: <b>".$usr."</b> with display name: <b>".$display."</b><br><br><a href='Session/session.php'>Go to your page</a>";// echo success
+        header("Location:Session/session.php");// echo success
       }
     }
 
@@ -35,14 +35,13 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") { // if method is post
   if ($type == "login") {// if type = login
     $usr = $_POST["usr"];
     $pwd  = $_POST["pwd"];
-    $prompt = "type = login<br>"/*.$usr." ".$pwd*/;
     $conn = new mysqli("localhost","root","root","project"); //Connect to the database 'project'
     $query = "SELECT * FROM users WHERE usr='$usr' AND pwd='$pwd'";// Here is the query
     $res = $conn->query($query);// Execute query in var "res"
     $check = $res->num_rows;// check for rows
     $row = $res->fetch_assoc();// get the row
     if ($check == 0) {// if username or password does not spit any matches
-      $prompt = "Invalid username or password";
+      echo "Invalid username or password";
     }
     if ($check == 1) { // if username and password does match
       $_SESSION["usr"] = $row["usr"];
@@ -54,18 +53,3 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") { // if method is post
 
 
  ?>
-
-
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Prompt</title>
-    <link rel="stylesheet" href="styles/main.css">
-  </head>
-  <body>
-    <div class="p">
-      <h1>Prompt</h1><br><br>
-      <?php echo $prompt ?>
-    </div>
-  </body>
-</html>
