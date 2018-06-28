3f83875b3306cadb34d01f13bc172374ce11b088
BuildTools <kabirdesarkar@gmail.com>
2018-06-28 11:01:46 +0530

    Username change capabilities 100% functional now

--- Session/session.php
+++ Session/session.php
@@ -21,22 +21,29 @@ else {
   </head>
   <body>
     <div class="main">
-    <span>Username: <input type="text" name="" value="<?php echo $usr; ?>" readonly> </span> <br>
-  <span>Display name: <input type="text" name="" value="<?php echo $display; ?>" readonly> </span><br><br>
+    <span>Username: <input type="text" name="usr" value="<?php echo $usr; ?>" readonly> </span> <br>
+  <span>Display name: <input type="text" name="display" value="<?php echo $display; ?>" readonly> </span><br><br>
     <button type="button"><a href="logout.php">Log Out</a></button>
     <button type="button" name="display" id="edit">Edit</button>
   </div>
-  <div class="modal">
+  <div class="modal"> <!-- MODAL DIVISION -->
     <div class="content">
-      <span class="div"><input type="text" name="usr" value="" placeholder="Username"></span><br>
-      <span class="div"><input type="text" name="display" value="" placeholder="Display Name"></span><br><br>
-      Verify Changes with password<br><br>
-      <span class="div"><input type="password" name="pwd" placeholder="Password"></span><br><br>
-      <button type="button" id="cancel">Cancel</button>
-      <button type="button" id="verify">Verify</button>
+      <form>
+        Edit user and Display name:<br><br>
+        <span class="div"><input type="text" name="usr" value="" placeholder="Username" id="usr"></span><br>
+        <span class="div"><input type="text" name="display" value="" placeholder="Display Name" id="display"></span><br>
+        <span class="p"></span>
+        <br><br>
+        Verify Changes with password<br><br>
+        <span class="div"><input type="password" name="pwd" placeholder="Password" id="pass"></span><br><br>
+        <button type="button" id="cancel">Cancel</button>
+        <button type="button" id="verify">Verify</button>
+    </form>
     </div>
   </div>
     <script type="text/javascript">
+
+
 
       var modal =document.getElementsByClassName('modal')[0];
       var modalContent = document.getElementsByClassName('content')[0];
@@ -46,9 +53,33 @@ else {
 
       edit_button.onclick = function () {
         modal.style.display = "block";
+        document.getElementsByName('usr')[1].value = document.getElementsByName('usr')[0].value;
+        document.getElementsByName('display')[2].value = document.getElementsByName('display')[0].value;
       };
       cancel_button.onclick = function () {
         modal.style.display = "none";
+      };
+      verify_button.onclick = function() {
+        var user = document.getElementById("usr").value;
+        var displayName = document.getElementById("display").value;
+        var pass = document.getElementById("pass").value;
+        $.ajax({
+          type:"POST",
+          url:"changes.php",
+          data: { usr:user,display:displayName,pwd:pass },
+          success: function (data) {
+            if (data == "1") {
+              modal.style.display="none";
+              window.location.reload(true);
+            }
+            else {
+              document.getElementsByClassName('p')[0].innerHTML = data;
+            }
+          },
+          error: function() {
+            document.getElementsByClassName('p')[0].innerHTML = "Some error occured in the server";
+          }
+        });
       };
 
     </script>
