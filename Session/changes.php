e5c4d3f3880a6794baa94b2e7a1f5908be432d4e
BuildTools <kabirdesarkar@gmail.com>
2018-06-29 18:05:18 +0530

    Password change feature implemented

--- Session/changes.php
+++ Session/changes.php
@@ -1,41 +1,63 @@
 <?php
+
   session_start();
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $old_user = $_SESSION["usr"];
-    $old_display = $_SESSION["display"];
-    $pass = $_POST["pwd"];
-    $new_user = $_POST["usr"];
-    $new_display = $_POST["display"];
+    if($_POST["type"] == "username"){
+      $old_user = $_SESSION["usr"];
+      $old_display = $_SESSION["display"];
+      $pass = $_POST["pwd"];
+      $new_user = $_POST["usr"];
+      $new_display = $_POST["display"];
 
-    $conn = new mysqli('localhost','root','root','project'); // Make a connection
-    $query = "SELECT * FROM users WHERE usr='$old_user'"; // Make the query variable
-    $res = $conn->query($query); // Result of the query
+      $conn = new mysqli('localhost','root','root','project'); // Make a connection
+      $query = "SELECT * FROM users WHERE usr='$old_user'"; // Make the query variable
+      $res = $conn->query($query); // Result of the query
 
-    $row = $res->fetch_assoc(); // fetch the associated row of the query result and return as array
-    $id = $row["ID"];
-    /*echo $row["usr"]." is your user<br>";
-    echo $row["display"]." is your display<br>";
-    echo $row["pwd"]." is your password<br>";*/
-    if ($pass == $row["pwd"]) {
-      if((strlen($new_user) < 6 || strlen($new_display) < 6) || (strlen($new_user) > 20 || strlen($new_display) > 20)) {
-        echo "New Username and display name must be at least 6 characters and at max 20 characters";
-      }
-      else {
-        $new_user_check = $conn->query("SELECT * FROM users WHERE NOT ID='$id' AND usr='$new_user'");
-        $new_user_check = $new_user_check->num_rows;
-        if ($new_user_check == 1) {
-          echo "Another user has the username: ".$new_user;
+      $row = $res->fetch_assoc(); // fetch the associated row of the query result and return as array
+      $id = $row["ID"];
+      /*echo $row["usr"]." is your user<br>";
+      echo $row["display"]." is your display<br>";
+      echo $row["pwd"]." is your password<br>";*/
+      if ($pass == $row["pwd"]) {
+        if((strlen($new_user) < 6 || strlen($new_display) < 6) || (strlen($new_user) > 20 || strlen($new_display) > 20)) {
+          echo "New Username and display name must be at least 6 characters and at max 20 characters";
         }
-        if ($new_user_check == 0) {
-        $query = "UPDATE users SET usr='$new_user', display='$new_display' WHERE ID=".$row["ID"];
+        else {
+          $new_user_check = $conn->query("SELECT * FROM users WHERE NOT ID='$id' AND usr='$new_user'");
+          $new_user_check = $new_user_check->num_rows;
+          if ($new_user_check == 1) {
+            echo "Another user has the username: ".$new_user;
+          }
+          if ($new_user_check == 0) {
+          $query = "UPDATE users SET usr='$new_user', display='$new_display' WHERE ID=".$row["ID"];
+          $res = $conn->query($query);
+          $_SESSION["usr"] = $new_user;
+          $_SESSION["display"] = $new_display;
+          echo "1";}}
+        }
+      else {
+        echo "Wrong password, try again.";
+      }} // for the username change
+
+      elseif ($_POST["type"] == "password") {
+        //echo "worked";
+        //echo $_SESSION["usr"];
+        $conn = new mysqli("localhost","root","root","project");
+        $query = "SELECT * FROM users WHERE usr='".$_SESSION["usr"]."'";
         $res = $conn->query($query);
-        $_SESSION["usr"] = $new_user;
-        $_SESSION["display"] = $new_display;
-        echo "1";}}
+        $row = $res->fetch_assoc();
+        $pass = $row["pwd"];
+        $new_pass = $_POST["np"];
+        if (!$_POST["op"] == $pass) {
+          echo "Incorrect password, try again";
+        }
+        else {
+          if (strlen($new_pass) < 6 || strlen($new_pass) > 20 ){echo "Password must be at lest 6 characters and most 20 characters";}
+          else {
+          $conn->query("UPDATE users SET pwd='$new_pass' WHERE ID=".$row["ID"]);
+          echo "1";}
+        }
       }
-    else {
-      echo "Wrong password, try again.";
-    }
 
   }
   else {
--- Session/session.php
+++ Session/session.php
@@ -24,9 +24,10 @@ else {
     <span>Username: <input type="text" name="usr" value="<?php echo $usr; ?>" readonly> </span> <br>
   <span>Display name: <input type="text" name="display" value="<?php echo $display; ?>" readonly> </span><br><br>
     <button type="button"><a href="logout.php">Log Out</a></button>
-    <button type="button" name="display" id="edit">Edit</button>
+    <button type="button" name="display" class="edit">Edit Profile</button> <!-- First edit button -->
+    <button type="button" name="pass" class="edit">Change Password</button> <!-- Second edit button -->
   </div>
-  <div class="modal"> <!-- MODAL DIVISION -->
+  <div class="modal"> <!-- MODAL DIVISION 1 -->
     <div class="content">
       <form>
         Edit user and Display name:<br><br>
@@ -36,40 +37,80 @@ else {
         <br><br>
         Verify Changes with password<br><br>
         <span class="div"><input type="password" name="pwd" placeholder="Password" id="pass"></span><br><br>
-        <button type="button" id="cancel">Cancel</button>
-        <button type="button" id="verify">Verify</button>
+        <button type="button" class="cancel">Cancel</button> <!-- First cancel button -->
+        <button type="button" class="verify">Verify</button> <!-- First verify button-->
+    </form>
+    </div>
+  </div>
+  <div class="modal"> <!-- MODAL DIVISION 2 -->
+    <div class="content" style="width:30%">
+      <form>
+        Change Password:<br><br>
+        <span class="div"><input type="password" name="password" placeholder="Old password"> </span><br><br>
+        <span class="div"><input type="password" name="password" placeholder="New password"> </span><br><br>
+        <span class="p"></span><br><br>
+        <button type="button" class="cancel">Cancel</button><!-- Second cancel button-->
+        <button type="button" class="verify">Change password</button> <!-- Second verify button-->
     </form>
     </div>
   </div>
     <script type="text/javascript">
 
 
-
-      var modal =document.getElementsByClassName('modal')[0];
+      var prompts = document.getElementsByClassName("p");
+      var modal =document.getElementsByClassName('modal');
       var modalContent = document.getElementsByClassName('content')[0];
-      var edit_button = document.getElementById('edit');
-      var cancel_button = document.getElementById('cancel');
-      var verify_button = document.getElementById('verify');
-
-      edit_button.onclick = function () {
-        modal.style.display = "block";
+      var edit_button = document.getElementsByClassName('edit');
+      var cancel_button = document.getElementsByClassName('cancel');
+      var verify_button = document.getElementsByClassName('verify');
+      edit_button[0].onclick = function () {
+        modal[0].style.display = "block";
         document.getElementsByName('usr')[1].value = document.getElementsByName('usr')[0].value;
         document.getElementsByName('display')[2].value = document.getElementsByName('display')[0].value;
       };
-      cancel_button.onclick = function () {
-        modal.style.display = "none";
+      edit_button[1].onclick = function () {
+        modal[1].style.display = "block";
+      }
+      cancel_button[0].onclick = function () {
+        modal[0].style.display = "none";
+        prompts[0].innerHTML = "";
       };
-      verify_button.onclick = function() {
+      cancel_button[1].onclick = function () {
+        modal[1].style.display = "none";
+        prompts[1].innerHTML = "";
+      };
+      verify_button[1].onclick = function () {
+        var old_pass = document.getElementsByName("password")[0].value;
+        var new_pass = document.getElementsByName("password")[1].value;
+        $.ajax ({
+          type:"POST",
+          url:"changes.php",
+          data: {op:old_pass,np:new_pass,type:"password"},
+          success: function (data) {
+            if (data == "1") {
+              modal[1].style.display = "none";
+              window.location.reload(true);
+            }
+            else {
+              prompts[1].innerHTML = data;
+            }
+          },
+          error: function () {
+            prompts[1].innerHTML = "Some error occured in trying to change your password";
+          }
+        });
+      };
+      verify_button[0].onclick = function() {
         var user = document.getElementById("usr").value;
         var displayName = document.getElementById("display").value;
         var pass = document.getElementById("pass").value;
         $.ajax({
           type:"POST",
           url:"changes.php",
-          data: { usr:user,display:displayName,pwd:pass },
+          data: { usr:user,display:displayName,pwd:pass,type:"username" },
           success: function (data) {
             if (data == "1") {
-              modal.style.display="none";
+              modal[0].style.display="none";
               window.location.reload(true);
             }
             else {
@@ -81,6 +122,7 @@ else {
           }
         });
       };
+
 
     </script>
   </body>
